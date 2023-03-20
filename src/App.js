9545af7a294f1780cdc366d6725d212518d3244f
Cesar Fernandez <chf05ster@gmail.com>
2023-03-20 00:17:35 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./styles.css";
+import lodash from 'lodash';
+import axios from 'axios';
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>This is a challenge</h1>
-      <h2>By Cesar Fernandez</h2>
+    <div className='wrapper'>
+      <div className='control'>
+        <input type='text' className='input'/>
+      </div>
+      <div className='list is-hoverable'>
+        hola
+      </div>
     </div>
   );
 }
